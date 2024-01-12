b65358975af2990e3a14704b4fd657cbf54ed68a
Himanjal saha <himanjalsaha97@gmail.com>
2024-01-12 12:40:43 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className=' text-red-500 text-lg flex justify-center items-center h-screen'>proclarity</div>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html",
+  "./src/**/*.{js,ts,jsx,tsx}",
+  "./App.{js,jsx,ts,tsx}",
+  "./components/**/*.{js,jsx}",
+  "./screens/**/*.{js,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
